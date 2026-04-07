862cdd2dbb4f13075c2349f74b541be89da5dcea
Tigas <0001158193@senaimgaluno.com.br>
2026-04-07 19:44:47 -0300

    Update urls.py

--- jogadores/urls.py
+++ jogadores/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_jogadores),
+    path('adicionar/', views.adicionar_jogador),
+    path('editar/<int:id>/', views.editar_jogador),
+    path('excluir/<int:id>/', views.excluir_jogador),
 ]
