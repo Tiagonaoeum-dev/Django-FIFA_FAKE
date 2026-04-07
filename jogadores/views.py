08e2388bbb92cdab5b31b444bd400384d0642764
Tigas <0001158193@senaimgaluno.com.br>
2026-04-07 19:44:10 -0300

    Update views.py

--- jogadores/views.py
+++ jogadores/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Jogador
+from .forms import JogadorForm
+
 
 def lista_jogadores(request):
     jogadores = Jogador.objects.all()
     return render(request, 'jogadores/lista.html', {'jogadores': jogadores})
+
+
+def adicionar_jogador(request):
+    if request.method == 'POST':
+        form = JogadorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = JogadorForm()
+
+    return render(request, 'jogadores/adicionar.html', {'form': form})
+
+
+def editar_jogador(request, id):
+    jogador = get_object_or_404(Jogador, id=id)
+
+    if request.method == 'POST':
+        form = JogadorForm(request.POST, instance=jogador)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = JogadorForm(instance=jogador)
+
+    return render(request, 'jogadores/adicionar.html', {'form': form})
+
+
+def excluir_jogador(request, id):
+    jogador = get_object_or_404(Jogador, id=id)
+
+    if request.method == 'POST':
+        jogador.delete()
+        return redirect('/')
+
+    return render(request, 'jogadores/excluir.html', {'jogador': jogador})
